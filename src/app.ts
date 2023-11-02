c4b315a6f267fe5f2f76e22f080d8a610f4eec26
Destiny-wei <weihaoming0418@gmail.com>
2023-11-02 19:53:58 +0800

    Deploy NFT Collection

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import * as dotenv from "dotenv";
 import { openWallet } from "./utils";
 import { readdir } from "fs/promises";
 import { updateMetadataFiles, uploadFolderToIPFS } from "./metadata";
+import { waitSeqno } from "./delay";
+import { NftCollection } from "./contracts/NftCollection";
 
 dotenv.config();
 
@@ -24,6 +26,20 @@ async function init() {
     console.log(
         `Successfully uploaded the metadata to ipfs: https://gateway.pinata.cloud/ipfs/${metadataIpfsHash}`
     );
+
+    console.log("Start deploy of nft collection...");
+    const collectionData = {
+        ownerAddress: wallet.contract.address,
+        royaltyPercent: 0.05, // 0.05 = 5%
+        royaltyAddress: wallet.contract.address,
+        nextItemIndex: 0,
+        collectionContentUrl: `ipfs://${metadataIpfsHash}/collection.json`,
+        commonContentUrl: `ipfs://${metadataIpfsHash}/`,
+    };
+    const collection = new NftCollection(collectionData);
+    let seqno = await collection.deploy(wallet);
+    console.log(`Collection deployed: ${collection.address}`);
+    await waitSeqno(seqno, wallet);
 }
 
 
